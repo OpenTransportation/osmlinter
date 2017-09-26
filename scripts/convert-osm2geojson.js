3efe3fdce974cc60c5195da3b044c6256fd497f9
Denis <carriere.denis@gmail.com>
2017-09-26 04:01:23 -0400

    prevent osm convert

--- scripts/convert-osm2geojson.js
+++ scripts/convert-osm2geojson.js
@@ -7,13 +7,15 @@ const featureCollection = require('@turf/helpers').featureCollection
 const osm2geojson = require('./osm2geojson')
 
 // Convert OSM XML to GeoJSON
-glob.sync(path.join(__dirname, '..', 'validators', '*', 'test', 'in', '*.osm')).forEach(filepath => {
-  const features = []
-  const stream = osm2geojson(filepath, { failEvents: true })
-  stream.on('data', feature => {
-    features.push(feature)
+if (process.env.REGEN) {
+  glob.sync(path.join(__dirname, '..', 'validators', '*', 'test', 'in', '*.osm')).forEach(filepath => {
+    const features = []
+    const stream = osm2geojson(filepath, { failEvents: true })
+    stream.on('data', feature => {
+      features.push(feature)
+    })
+    stream.on('end', () => {
+      write.sync(filepath.replace('.osm', '.geojson'), featureCollection(features))
+    })
   })
-  stream.on('end', () => {
-    write.sync(filepath.replace('.osm', '.geojson'), featureCollection(features))
-  })
-})
+}
