e049f2a32be916288046f3aba05f6a2365d0b757
Denis <carriere.denis@gmail.com>
2017-09-25 23:56:45 -0400

    Replace jest with tap

--- validators/impossible-angle/test.js
+++ validators/impossible-angle/test.js
@@ -1,27 +1,34 @@
+const test = require('tape')
 const path = require('path')
 const glob = require('glob')
 const load = require('load-json-file')
 const lineString = require('@turf/helpers').lineString
 const impossibleAngle = require('../..').impossibleAngle
 
-test('impossible-angle', () => {
+test('impossible-angle', t => {
   // True
   glob.sync(path.join(__dirname, 'test', 'true', '*.geojson')).forEach(filepath => {
     const line = load.sync(filepath)
-    const options = line.properties || {}
-    expect(impossibleAngle(line, options)).toBeTruthy()
+    t.true(impossibleAngle(line, line.properties))
   })
   // False
   glob.sync(path.join(__dirname, 'test', 'false', '*.geojson')).forEach(filepath => {
     const line = load.sync(filepath)
-    const options = line.properties || {}
-    expect(impossibleAngle(line, options)).toBeFalsy()
+    t.false(impossibleAngle(line, line.properties))
   })
+  t.end()
 })
 
-test('impossible-angle -- GeoJSON', () => {
+test('impossible-angle -- GeoJSON', t => {
   const line = lineString([[5, 5], [5, 6], [3, 4]])
 
-  expect(impossibleAngle(line)).toBeTruthy()
-  expect(impossibleAngle(line.geometry)).toBeTruthy()
+  t.true(impossibleAngle(line))
+  t.true(impossibleAngle(line.geometry))
+  t.false(impossibleAngle(line.geometry, { threshold: 60 }))
+  t.end()
+})
+
+test('impossible-angle -- throws errors', t => {
+  t.throws(() => impossibleAngle(null), /line is required/, 'line is required')
+  t.end()
 })
--- validators/utils/test.js
+++ validators/utils/test.js
@@ -1,5 +1,7 @@
+const test = require('tape')
 const findAngle = require('../..').findAngle
 
-test('utils -- findAngle', () => {
-  expect(findAngle([5, 5], [5, 6], [3, 4])).toBe(45)
+test('utils -- findAngle', t => {
+  t.equal(findAngle([5, 5], [5, 6], [3, 4]), 45)
+  t.end()
 })
