c153ce99f3b1303ef1244f3f6aaf45146c1e6d70
Denis <carriere.denis@gmail.com>
2017-11-25 21:07:46 -0800

    Fix test

--- src/get-angle/test.js
+++ src/get-angle/test.js
@@ -1,7 +1,8 @@
 import test from 'tape'
+import { round } from '@turf/helpers'
 import getAngle from './'
 
 test('get-angle', t => {
-  t.equal(getAngle([5, 5], [5, 6], [3, 4]), 45)
+  t.equal(round(getAngle([5, 5], [5, 6], [3, 4])), 45)
   t.end()
 })
